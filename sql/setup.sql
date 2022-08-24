67c9fd5ec55bfcdb04f49d7908acda85f589026d
olivia pasion <oap9820@gmail.com>
2022-08-23 18:01:33 -0700

    started #POST new user test

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS todo_users;
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todo_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_has VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  description VARCHAR NOT NULL,
+  done BOOLEAN NOT NULL DEFAULT(false)
+);
